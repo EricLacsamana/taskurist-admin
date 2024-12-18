b3ebec23e85eafb52473a96425cdea9034ef8b7d
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-18 13:12:53 +0800

    implement tanstack react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './css/style.css';
 import './css/satoshi.css';
 // import '/node_modules/jsvectormap/dist/css/jsvectormap.css';
 // import 'flatpickr/dist/flatpickr.min.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
