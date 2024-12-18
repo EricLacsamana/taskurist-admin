fd64355774a70f501895702be44e59db6aa521d6
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-18 16:18:38 +0800

    rename to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Calendar from './pages/Calendar';
 import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
+import Dashboard from './pages/Dashboard';
 import FormElements from './pages/Form/FormElements';
 import FormLayout from './pages/Form/FormLayout';
 import Profile from './pages/Profile';
@@ -61,8 +61,8 @@ function App() {
           index
           element={
             <>
-              <PageTitle title="eCommerce Dashboard | Taskurist - Tailwind CSS Admin Dashboard Template" />
-              <ECommerce />
+              <PageTitle title="Dashboard | Taskurist - Tailwind CSS Admin Dashboard Template" />
+              <Dashboard />
             </>
           }
         />
