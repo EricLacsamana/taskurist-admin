6d691338faad9cbce5645c8f607b22eb9e071390
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-19 15:51:21 +0800

    update bread crumb

--- src/components/Breadcrumbs/Breadcrumb.jsx
+++ src/components/Breadcrumbs/Breadcrumb.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Breadcrumb = ({ pageName = '' }) => {
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-start">
+      {/* <h2 className="text-title-md2 font-semibold text-black dark:text-white">
         {pageName}
-      </h2>
+      </h2> */}
 
       <nav>
         <ol className="flex items-center gap-2">
