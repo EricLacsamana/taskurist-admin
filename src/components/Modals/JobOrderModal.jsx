d93bc29ba30ddefd1c0f56e3f184801e5a48d8da
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-20 11:07:56 +0800

    job order modal

--- src/components/Modals/JobOrderModal.jsx
+++ src/components/Modals/JobOrderModal.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import JobOrderForm from "../../pages/Form/JobOrderForm";
 
-export const JobOrderModal = ({ closeModal, isOpen }) => {
+export const JobOrderModal = ({ onClose, isOpen }) => {
   // Early return if the modal is not open
   if (!isOpen) return null;
+
+  // Close the modal when the Escape key is pressed
+  useEffect(() => {
+    const handleEscKey = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    // Attach the event listener for the Escape key
+    document.addEventListener("keydown", handleEscKey);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [onClose]);
 
   return (
     <div
       className="modal-container fixed inset-0 flex justify-center items-center z-9999 bg-gray-500 bg-opacity-50"
       onClick={(e) => {
-        if (e.target.className === "modal-container") closeModal();
+        if (e.target.className === "modal-container") onClose();
       }}
+      aria-hidden={!isOpen}
     >
-      <div className="modal rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark overflow-auto max-w-lg w-full relative">
+      <div
+        className="modal rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark overflow-auto max-w-4xl w-full relative"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
         {/* Close button with absolute positioning */}
-        <strong 
-          className="text-xl align-center cursor-pointer absolute top-4 right-4 dark:text-white" 
-          onClick={closeModal}
+        <button
+          className="text-xl align-center cursor-pointer absolute top-4 right-4 dark:text-white"
+          onClick={onClose}
+          aria-label="Close Modal"
         >
           &times;
-        </strong>
-        
+        </button>
+
         <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-          <h3 className="font-medium text-black dark:text-white">Job Order Form</h3>
+          <h3 className="font-medium text-black dark:text-white" id="modalTitle">
+            Job Order Form
+          </h3>
         </div>
 
         {/* Modal content section with scrollable area */}
@@ -34,3 +60,4 @@ export const JobOrderModal = ({ closeModal, isOpen }) => {
     </div>
   );
 };
+  
