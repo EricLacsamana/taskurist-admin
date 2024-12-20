1fb450c221e79fc85d8f35dc7b918b76f22c17a1
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-20 11:08:48 +0800

    job orders page

--- src/pages/JobOrders.jsx
+++ src/pages/JobOrders.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
 import JobOrdersTable from '../components/JobOrdersTable';
+import { JobOrderModal } from '../components/Modals/JobOrderModal';
+import FloatingActionButton from '../components/FloatingActionButton';
+
 
 const JobOrders = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleOpenModal = () => setIsOpen(true);
+    const handleCloseModal = () => setIsOpen(false)
     return (
     <>
         <Breadcrumb pageName="Job Orders" />
@@ -9,7 +16,10 @@ const JobOrders = () => {
         <div className="flex flex-col gap-10">
 
             <JobOrdersTable />
+            <FloatingActionButton onClick={handleOpenModal} />
+            <JobOrderModal isOpen={isOpen} onClose={handleCloseModal} />
         </div>
+   
     </>
     );
 };
